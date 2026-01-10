5275bb0e7e406dbe2d25e7aade8a98565a00b15d
Ramadhan R. <riobaramadhan@gmail.com>
2026-01-10 17:01:44 +0300

    Add Supabase client setup

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// supabase.js
+const SUPABASE_URL = "https://sydynixecbokrttohcvr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Re78GtQWMIxAvz8z0kX3mA_hhL0TC60";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
